b25c024a487b7c5dc1f7f7118e7d7b1500988004
ZUHOWKS <jojo.vilard38@gmail.com>
2026-03-26 09:08:01 +0100

    Remove API fetcher and error classes

--- apps/ferrisquote-webapp/src/api/api-error.ts
+++
@@ -1,9 +0,0 @@
-export class ApiError extends Error {
-  constructor(
-    public readonly status: number,
-    public readonly body: string,
-  ) {
-    super(`API error ${status}: ${body}`)
-    this.name = "ApiError"
-  }
-}
